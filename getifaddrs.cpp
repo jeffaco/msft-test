09d248d82b1397700b8da92e42c0fe16338ef01e
Jeff Coffler <jeff@taltos.com>
2017-05-05 15:50:17 -0700

    getifaddrs: Code to look up network interfaces

---
+++ getifaddrs.cpp
@@ -0,0 +1,31 @@
+// If you want to use "nulptr" (below), then compile with:
+//
+//   g++ -std=c++11 -o getifaddrs getifaddrs.cpp
+//
+// Otherwise, the -std=c++11 qualifier is not needed.
+
+#include <sys/types.h>
+#include <ifaddrs.h>
+
+#include <string>
+#include <set>
+#include <iostream>
+
+int main()
+{
+    struct ifaddrs* ifaddr = nullptr;
+    if (getifaddrs(&ifaddr) == -1) {
+        std::cout << "Error: getifaddrs failed" << std::endl;
+        return 1;
+    }
+
+    std::set<std::string> names;
+    for (struct ifaddrs* ifa = ifaddr; ifa != NULL /* nullptr */; ifa = ifa->ifa_next) {
+        if (ifa->ifa_name != NULL /* nullptr */) {
+            std::cout << "Interface: " << ifa->ifa_name << std::endl;
+        }
+    }
+
+    freeifaddrs(ifaddr);
+    return 0;
+}
